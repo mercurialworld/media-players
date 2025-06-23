01decd43071a2f5ab2cebaac4b307f1ef1959823
ポッチャマ <mercworld@pocha.moe>
2025-06-23 01:35:12 -0400

    fix: cry in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@project-types": path.resolve(__dirname, "./src/types"),
+            "@components": path.resolve(__dirname, "./src/components/*"),
+            "@hooks": path.resolve(__dirname, "./src/hooks/*"),
+            "@reducers": path.resolve(__dirname, "./src/reducers/*"),
+            "@contexts": path.resolve(__dirname, "./src/contexts/*"),
+            "@utils": path.resolve(__dirname, "./src/utils/*"),
+        },
+    },
+});
