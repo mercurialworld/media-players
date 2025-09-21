5c2ee9b57bce2909521cc6a20c2ab12192eccdf7
ポッチャマ <mercworld@pocha.moe>
2025-09-21 16:25:40 -0400

    feat: update site
    i'm this close to archiving it :3

--- src/components/Header/Information.tsx
+++ src/components/Header/Information.tsx
@@ -1,59 +1,22 @@
-import Settings from "@components/Settings/Settings";
-import { Group, Image, Text, Title } from "@mantine/core";
+import { Text } from "@mantine/core";
 import classes from "@styles/Information.module.css";
 
 const Info = () => {
     return (
         <div className={classes.info}>
-            <div className={classes.settings}>
-                <Settings />
-            </div>
-            <Group
-                className={classes.title}
-                gap="xs"
-                align="center"
-                justify="center"
-            >
-                <Image
-                    darkHidden
-                    h="calc(2rem + 2.5vw)"
-                    w="auto"
-                    fit="contain"
-                    alt="The logo for Music Presence."
-                    src="https://raw.githubusercontent.com/music-presence/icons/refs/heads/master/dist/tray-light.png"
-                />
-                <Image
-                    lightHidden
-                    h="calc(2rem + 2.5vw)"
-                    w="auto"
-                    fit="contain"
-                    alt="The logo for Music Presence."
-                    src="https://raw.githubusercontent.com/music-presence/icons/refs/heads/master/dist/tray-dark.png"
-                />
-                <Title order={1}>Media Players</Title>
-            </Group>
-            <br />
             <div>
                 <Text>
-                    This is a list of all media players that Music Presence can
-                    detect. It is always kept up to date from official sources.
+                    Temporarily taking this down as there have been too many questions about it, implying it's a bit misleading. It'll come back in two weeks or so with clearer documentation.
                 </Text>
                 <br />
                 <Text>
-                    Note that some media players listed might need additional setup.
-                    Check the{" "}
-                    <a href="https://github.com/ungive/discord-music-presence/blob/master/documentation/supported-media-players.md#additional-notes">
-                        Additional Notes
-                    </a>{" "}
-                    section for more information.
+                    If you need to know if your media player is supported right now though, you can visit <a href="https://live.musicpresence.app/v3/players.json">https://live.musicpresence.app/v3/players.json</a> and search the JSON it gives you for the name of your player. <br /> <strong>Note that the browser extension is not released yet, so for now, disregard any of the "web" entries.</strong>
                 </Text>
                 <br />
                 <Text>
-                    Don't see your media player?{" "}
-                    <a href="https://github.com/ungive/discord-music-presence/blob/master/documentation/troubleshooting.md#if-your-media-player-is-still-not-detected">
-                        Submit it!
-                    </a>
+                    This site might also be on the Internet Archive, probably.
                 </Text>
+                <br />
             </div>
         </div>
     );
